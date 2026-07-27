ab9a6c403c05efc445d6041dc84b2e7a84320600
Anna_Emet <karanjann.codingal@gmmail.com>
2026-07-27 20:35:24 +0300

    create a simple Python program that prints a daily routine step by step. They will learn how the print() function displays text on the screen and how arithmetic can also be printed as output.The program will print a title, show routine steps like waking up, going to school, playing, and studying, and finally calculate the total study time in a week

---
+++ dailyroutine.py
@@ -0,0 +1,22 @@
+'''
+Assignment: My Daily Routine Printer
+In this assignment, students will create a simple Python program that prints a daily routine step by step. They will learn how the print() function displays text on the screen and how arithmetic can also be printed as output.The program will print a title, show routine steps like waking up, going to school, playing, and studying, and finally calculate the total study time in a week.
+
+'''
+# Printing the title
+print("My Daily Routine")
+
+# Step 1: Waking up
+print("I wake up at 7 AM.")
+
+# Step 2: Going to school
+print("I go to school at 8 AM.")
+
+# Step 3: Playing after school
+print("I play with my friends in the evening.")
+
+# Step 4: Studying
+print("I study for 2 hours at night.")
+
+# Calculating total study time in a week
+print("Total study hours in a week:", 2 * 7)
